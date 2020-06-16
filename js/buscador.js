2ad54ca66b7a24e7e8dabe8434f701070b077e57
Juan Camilo Vélez <jcavelez@gmail.com>
2020-06-15 19:47:48 -0500

    manejo de errores, antes de localstorage

---
+++ js/buscador.js
@@ -0,0 +1,109 @@
+const $buscador = document.getElementById('buscador')
+const $results = document.getElementById('main__results')
+const $resultsCarousel = document.getElementById('resultsCarousel')
+
+
+
+//Cuando al formulario se le de enter se le quita el comportamiento por defecto
+//que viene con el elemento que es recargar la página
+//luego se activa el carrusel de resultlados
+$buscador.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  $results.classList.add('main__results--active')
+  // Borramos lo que hay en el carrusel de resultados para poner el loader
+  emptyHTMLCollection($resultsCarousel)
+  //agregamos un elemento (just for fun (?))
+  let $loader = document.createElement('img')
+  setAttributes($loader, {
+    src: 'assets/img/loader.gif',
+    width: 50,
+    height: 50,
+  })
+
+  $resultsCarousel.append($loader)
+  let data = new FormData($buscador)
+  let searchResults = await getData(`${API_URL}limit=10&query_term=${data.get('movie')}`)
+  // Borramos el $loader
+  emptyHTMLCollection($resultsCarousel)
+  if (searchResults)
+  {
+    searchResults.forEach((movie) => {
+      console.log(movie.title)
+      let item = createCarouselItemTemplate(movie)
+      insertHTML($resultsCarousel, item)
+    })
+  }
+  else
+  {
+    insertHTML($resultsCarousel, '<div style="font-size: 20px">Título no encontrado</div>')
+  }
+
+})
+
+//para que cuando se de clic en el buscador, desaparezca el carrusel de
+//resultados
+// $buscador.addEventListener('click', (event) => {
+//   $results.classList.remove('main__results--active')
+// })
+
+function setAttributes($element, attributes)
+{
+  for (let attribute in attributes)
+  {
+    $element.setAttribute(attribute, attributes[attribute])
+  }
+}
+
+async function getData(API)
+{
+  const response = await fetch(`${API}`)
+  const movies = await response.json()
+  console.log(`Llegaron las pelis de la búsqueda`, movies.data.movies)
+  return movies.data.movies
+}
+
+async function getMovieDetails(API)
+{
+  const response = await fetch(`${API}`)
+  const movies = await response.json()
+  console.log(`Llegaron las pelis de la búsqueda`, movies.data.movie)
+  return movies.data.movie
+}
+
+
+function createCarouselItemTemplate(movie)
+{
+  return `
+  <div class="carousel-item" data-id="${movie.id}">
+    <img class="carousel-item__img" src="${movie.medium_cover_image}" alt="${movie.title}" id="${movie.id}">
+    <div class="carousel-item__details">
+      <div>
+        <img class="carousel-item__details--img" src="./assets/img/play-icon.png" alt="Reproducir">
+        <img class="carousel-item__details--img" src="./assets/img/plus-icon.png" alt="Agregar">
+      </div>
+      <p class="carousel-item__details--title">${movie.title}</p>
+      <p class="carousel-item__details--subtitle">${movie.year} ‧ ${movie.genres} ‧ Rating ${movie.rating}</p>
+    </div>
+  </div>
+  `
+}
+
+function insertHTML($selector, template)
+{
+  const html = document.implementation.createHTMLDocument()
+  html.body.innerHTML = template
+  movieElement = html.body.children[0]
+  $selector.append(movieElement)
+  const imageMovieElement = movieElement.querySelector('.carousel-item__img')
+  imageMovieElement.addEventListener('load', () => { imageMovieElement.classList.add('fadeIn')} )
+  movieElement.addEventListener('click', () => { showModal(parseInt(movieElement.dataset.id, 10)) })
+}
+
+function emptyHTMLCollection($HTMLCollection)
+{
+  let sizeHTMLCollection = $HTMLCollection.children.length
+  for (let i = 0; i < sizeHTMLCollection; i++)
+  {
+    $resultsCarousel.children[0].remove()
+  }
+}
--- js/movie-list.js
+++ js/movie-list.js
@@ -1,12 +1,19 @@
 const API_URL = 'https://yts.mx/api/v2/list_movies.json?with_images=true&'
+const MOVIE_DETAILS_URL = 'https://yts.mx/api/v2/movie_details.json?'
 
-const GENRES = {action: 'Acción',
+const GENRES = {
+              comedy: 'Comedia',
+              action: 'Acción',
               history: 'Historia',
               thriller: 'Suspenso',
               }
 
 const $listsContainer = document.getElementById('lists')
+const $overlay = document.getElementById('overlay')
+const $modal = document.getElementById('modal')
 
+
+//Render de carrouseles
 for (let genre in GENRES)
 {
   let templateTitles = `
@@ -34,26 +41,26 @@ for (let genre in GENRES)
 
 }
 
-
+//Render de item de cada carrusel
 for (let genre in GENRES)
 {
   async function fillCarousel()
   {
     async function getMovies(genre)
     {
-
-      async function askAPI()
-      {
+      let movies
+      try {
         const response = await fetch(`${API_URL}genre=${genre}`)
-        const movies = await response.json()
+        movies = await response.json()
         console.log(`Llegaron las pelis de ${genre} por async await`, movies.data)
         return movies.data.movies
+      } catch (e) {
+        console.log(e)
+        movies = [{id: 'undefined', title: 'Not found', medium_cover_image: './assets/img/404-error-page-not-found-miss-paper-with-white-vector.jpg'}]
+        return movies
+      } finally {
+
       }
-
-      const movies = await askAPI(API_URL)
-
-      return movies
-
     }
 
     const MOVIES = await getMovies(genre)
@@ -61,9 +68,10 @@ for (let genre in GENRES)
     $carousel.children[0].remove()
     //console.log(MOVIES)
     MOVIES.forEach((movie) => {
+      let movieElement
       const templateItem = `
-      <div class="carousel-item">
-        <img class="carousel-item__img" src="${movie.medium_cover_image}" alt="${movie.title}" id="${movie.id}">
+      <div class="carousel-item" data-id="${movie.id}">
+        <img class="carousel-item__img" src="${movie.medium_cover_image}" alt="${movie.title}">
         <div class="carousel-item__details">
           <div>
             <img class="carousel-item__details--img" src="./assets/img/play-icon.png" alt="Reproducir">
@@ -77,12 +85,92 @@ for (let genre in GENRES)
 
       const html = document.implementation.createHTMLDocument()
       html.body.innerHTML = templateItem
-      $carousel.append(html.body.children[0])
-
+      movieElement = html.body.children[0]
+      $carousel.append(movieElement)
+      const imageMovieElement = movieElement.querySelector('.carousel-item__img')
+      imageMovieElement.addEventListener('load', () => { imageMovieElement.classList.add('fadeIn')} )
+      movieElement.addEventListener('click', () => { showModal(parseInt(movieElement.dataset.id, 10)) })
     })
-
   }
 
-
   fillCarousel()
+}
+
+async function showModal(movieId)
+{
+  $overlay.classList.add('active');
+
+  console.log(`Vamos a mostrar la pelicula ID ${movieId}`)
+  const $modalContent = document.querySelector('.modal-content')
+  let movieData
+  let movieElement
+
+
+  movieData = await getMovieDetails(`${MOVIE_DETAILS_URL}movie_id=${movieId}`)
+
+  const movieModalTemplate = createModalTemplate(movieData)
+  const movieHTMLCollection = document.implementation.createHTMLDocument()
+  movieHTMLCollection.body.innerHTML = movieModalTemplate
+  movieElement = movieHTMLCollection.body.children[0]
+
+  let sizeModalContent = $modalContent.children.length
+  for (let i = 0; i < sizeModalContent; i++)
+  {
+    $modalContent.children[0].remove()
+  }
+
+  $modalContent.append(movieElement)
+
+  $modal.style.animation = 'modalIn .8s forwards';
+
+  const $closeModal = document.getElementById('closeModal')
+  $closeModal.addEventListener('click', hideModal)
+  $overlay.addEventListener('click', hideModal)
+}
+
+function hideModal() {
+  $overlay.classList.remove('active');
+  $modal.style.animation = 'modalOut .5s forwards';
+}
+
+function setAttributes($element, attributes)
+{
+  for(let attr in attrbutes)
+  {
+    $element.setAttributes(attr, attributes[key])
+  }
+}
+
+function createModalTemplate(movie)
+{
+  let movieGenres = ''
+
+  for(genre in movie.genres)
+  {
+    movieGenres = `${movie.genres[genre]}, ${movieGenres}`
+  }
+
+  console.log(movieGenres)
+
+  return `
+  <div class="modal-content">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="closeModal">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <h4 class="modal-title" id="myModalLabel">${movie.title}</h4>
+    </div>
+    <div class="modal-main">
+      <img class="modal-img" src="${movie.medium_cover_image}" alt="">
+      <p class="modal-desc">${movie.description_full}
+      </p>
+    </div>
+    <div class="modal-details info">
+        <span class="sub">Géneros: </span> <span>${movieGenres}</span>
+        <span class="sub">Idioma: </span> <span>${movie.language}</span>
+        <span class="sub">Año: </span> <span>${movie.year}</span>
+        <span class="sub">Rating IMBd: </span> <span>${movie.rating}</span>
+    </div>
+  </div>
+  `
 }
